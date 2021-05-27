7a66203f7661e23cbfc1a784bcedd97b8b9aa378
kio <un19451024@gmail.com>
2021-05-27 18:06:04 +0900

    [Feat] TableView, ColletionView 구현

---
+++ OpenMarket/OpenMarket/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by kio on 2021/05/27.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
